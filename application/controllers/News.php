e3130f7a8efb5cc8b785d859d633ad36d5f0aa02
Manish Kumar <kumarmanishbit@rediffmail.com>
2015-08-06 18:22:59 +0530

    Second Commit

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -33,4 +33,28 @@ class News extends CI_Controller {
         $this->load->view('news/view', $data);
         $this->load->view('footer');
         }
+
+        public function create()
+{
+    $this->load->helper('form');
+    $this->load->library('form_validation');
+
+    $data['title'] = 'Create a news item';
+
+    $this->form_validation->set_rules('title', 'Title', 'required');
+    $this->form_validation->set_rules('text', 'text', 'required');
+
+    if ($this->form_validation->run() === FALSE)
+    {
+        $this->load->view('header', $data);
+        $this->load->view('news/create');
+        $this->load->view('footer');
+
+    }
+    else
+    {
+        $this->news_model->set_news();
+        $this->load->view('news/success');
+    }
+}
 }
